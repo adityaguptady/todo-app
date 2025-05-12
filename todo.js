3c71b93b32eb81363d290cde23995a50666ddac9
Aditya Gupta <adityaguptady@gmail.com>
2025-05-12 18:12:34 +0530

    Added add to do functionality

--- todo.js
+++ todo.js
@@ -1,1 +1,21 @@
 console.log("Hello")
+
+var todoListArray = []
+
+function addTodo()
+{
+    console.log("Add Todo!!")
+    let todoInput = document.getElementById("todoInput")
+    console.log(todoInput)
+    console.log(todoInput.value)
+    if(todoInput.value.trim() == "")
+    {
+        alert("Todo title cannot be blank!!!")
+    }
+    else
+    {
+        console.log(todoListArray)
+        todoListArray.push(todoInput.value)
+        console.log(todoListArray)
+    }
+}
