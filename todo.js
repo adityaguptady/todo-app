95430e6f88e12448f08622c3181b7af5160f3c87
Aditya Gupta <adityaguptady@gmail.com>
2025-05-17 18:15:47 +0530

    Added complete a todo funtionality

--- todo.js
+++ todo.js
@@ -1,7 +1,31 @@
-console.log("Hello")
-
-var todoListArray = []
+$(document).ready(function() 
+{
+    updateFrontend()
+});
+// console.log("Hello")
 var count = 0
+var todoListArray = [
+    {
+        id: ++count,
+        todo: "Todo 1",
+        completed: false
+    }, 
+    {
+        id: ++count,
+        todo: "Todo 2",
+        completed: false
+    },
+    {
+        id: ++count,
+        todo: "Todo 3",
+        completed: false
+    },
+    {
+        id: ++count,
+        todo: "Todo 4",
+        completed: false
+    }
+]
 
 function addTodo()
 {
@@ -23,12 +47,46 @@ function addTodo()
         })
         console.log(todoListArray)
     }
+    updateFrontend()
+}
+
+function updateFrontend()
+{
     let todoList = document.getElementById("todoList")
     todoList.innerHTML = ""
     for(let index = 0; index<todoListArray.length; index++)
     {
         // todoList.innerHTML += "<li>"+todoListArray[index]+"</li>"
-        todoList.innerHTML += "<li id="+todoListArray[index].id+"><input type='checkbox'/><label>"+todoListArray[index].todo+"</label><button>Edit</button><button>Delete</button></li>"
-        
+        if(todoListArray[index].completed)
+            todoList.innerHTML += "<li id="+todoListArray[index].id+"><input type='checkbox'  onclick='onTodoComplete(this, "+todoListArray[index].id+")' checked/><label>"+todoListArray[index].todo+"</label><button>Edit</button><button>Delete</button></li>"
+        else
+            todoList.innerHTML += "<li id="+todoListArray[index].id+"><input type='checkbox'  onclick='onTodoComplete(this, "+todoListArray[index].id+")'/><label>"+todoListArray[index].todo+"</label><button>Edit</button><button>Delete</button></li>"
     }
+}
+
+function onTodoComplete(checkbox, todoID)
+{
+    // console.log("Checked change!", checkbox.checked)
+    // console.log("Todo ID:", todoID)
+    //find the correct object from the array based on id
+    //update the object to change the checked property
+    //update the array
+    console.log(todoListArray)
+    todoListArray = todoListArray.map((todoObj)=>
+    {
+        if(todoObj.id == todoID)
+        {
+            //console.log("Found!!!", todoObj.todo)
+            // if(checkbox.checked)
+            //     todoObj.checked = true
+            // else
+            //     todoObj.checked = false
+
+            todoObj.completed = checkbox.checked
+        }
+        return todoObj
+    })
+    console.log(todoListArray)
+    //update the Frontend
+    updateFrontend()
 }
